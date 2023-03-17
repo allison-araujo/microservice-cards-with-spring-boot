7f2f423d312a2121b965ca2af263d326b1892af9
allisonvinicius <allisonvinicius63@gmail.com>
2023-03-17 10:11:26 -0400

    create: include dependency and annotation rabbitmq

--- mscards/src/main/java/io/github/allison/cards/mscards/MscardsApplication.java
+++ mscards/src/main/java/io/github/allison/cards/mscards/MscardsApplication.java
@@ -1,17 +1,16 @@
 package io.github.allison.cards.mscards;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableRabbit
 public class MscardsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscardsApplication.class, args);
 	}
-
-
-
 }
